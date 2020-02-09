400738ccf60110348b7e62316f8809f1c45a8950
BISWAS, ANKIT <ANKIT.BISWAS@cerner.com>
2020-02-09 11:39:38 +0530

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,12 +10,17 @@ class Quiz extends Component{
         this.state={quiz_position:1}
 
     }
+
+    showNextQuestion(){
+        const positionNew =this.state.quiz_position+1
+        this.setState({quiz_position:positionNew})
+    }
     
     render(){
         const isQuizEnd = ((this.state.quiz_position-1)===quizData.quiz_questions.length)       
         return<div>
             {isQuizEnd ? <QuizEnd/> :
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>}
+            <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>}
         </div> 
                 {/* <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>  */}
 
